af0e51b000e7f2fd3d10548afde080979571777d
BryanNguyen03 <bryannguyen051012@gmail.com>
2025-11-10 20:43:03 -0500

    feat: Added initial Auth service with login endpoint

---
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -0,0 +1,36 @@
+package com.pm.authservice.service;
+
+import com.pm.authservice.model.User;
+import com.pm.authservice.dto.LoginRequestDTO;
+import com.pm.authservice.util.JwtUtil;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+    // Dependency injections for authentication
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+        this.jwtUtil = jwtUtil;
+    }
+
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
+        // Get token if email exists and password matches
+        Optional<String> token = userService
+                .findByEmail(loginRequestDTO.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(),
+                        u.getPassword()))
+                // map to token if both checks pass
+                .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
+
+        return token;
+    }
+}
